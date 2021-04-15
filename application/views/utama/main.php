cac32ceaca99d295e0e44489dc953d4416259e8e
raydewa03 <raydewa03@gmail.com>
2021-04-15 22:49:32 +0700

    Pengaduan Masyarakat CodeIgniter

---
+++ application/views/utama/main.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Template Mo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+    <title>Sistem Pengaduan Masyarakat</title>
+    <!--
+
+ART FACTORY
+
+https://templatemo.com/tm-537-art-factory
+
+-->
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/utama/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/utama/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="assets/utama/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="assets/utama/css/owl-carousel.css">
+    <link rel="shortcut icon" href="assets/utama/pengaduan.jpg" type="image/x-icon">
+
+</head>
+
+<body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+    <!-- ***** Preloader End ***** -->
+
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="#" class="logo">Aplikasi Pengaduan Masyarakat</a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
+
+
+    <!-- ***** Welcome Area Start ***** -->
+    <div class="welcome-area" id="welcome">
+
+        <!-- ***** Header Text Start ***** -->
+        <div class="header-text">
+            <div class="container">
+                <div class="row">
+                    <div class="left-text col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                        <h1>Layanan Pengaduan <strong>E - Ngadu</strong></h1>
+                        <p>E - Ngadu adalah aplikasi pengaduan masyarakat berbasis web yang diciptakan untuk menampung keluhan anda</p>
+                        <a href="<?= base_url('Auth/LoginController') ?>" class="main-button-slider">Login</a>
+                        <a href="#about" class="main-button-slider">About</a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                        <img src="assets/utama/images/main.png" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ***** Header Text End ***** -->
+    </div>
+    <!-- ***** Welcome Area End ***** -->
+
+
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
+                    <img src="assets/utama/images/about.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+                <div class="right-text col-lg-5 col-md-12 col-sm-12 mobile-top-fix">
+                    <div class="left-heading">
+                        <h5>About Rayhan Sadewa</h5>
+                    </div>
+                    <div class="left-text">
+                        <p>Aplikasi ini dibuat oleh Rayhan Sadewa sebagai tugas atau project untuk ujikom.
+                            Aplikasi ini membuat saya pusing, jadi tolong diterima apa adanya. Jangan kebanyakan
+                            ngadu karna mengadu itu tidak baik.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Big Item End ***** -->
+
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 col-sm-12">
+                    <p class="copyright">Copyright &copy; 2021 Rayhan Sadewa</p>
+                </div>
+                <div class="col-lg-5 col-md-12 col-sm-12">
+                    <ul class="social">
+                        <li><a href="https://www.facebook.com/rayhan.sadewa.75/"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="https://www.instagram.com/rayhansadewa/"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/rayhan-sadewa-5756b01b7/"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="assets/utama/js/jquery-2.1.0.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="assets/utama/js/popper.js"></script>
+    <script src="assets/utama/js/bootstrap.min.js"></script>
+
+    <!-- Plugins -->
+    <script src="assets/utama/js/owl-carousel.js"></script>
+    <script src="assets/utama/js/scrollreveal.min.js"></script>
+    <script src="assets/utama/js/waypoints.min.js"></script>
+    <script src="assets/utama/js/jquery.counterup.min.js"></script>
+    <script src="assets/utama/js/imgfix.min.js"></script>
+
+    <!-- Global Init -->
+    <script src="assets/utama/js/custom.js"></script>
+
+</body>
+
+</html>
